c770abf81aa5635bb2a7f3fb00681c3f1de3be68
SriHarshaSabbineni <sabbinenisriharsha8490@gmail.com>
2020-05-22 07:02:26 +0530

    added password digest to user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
       name: params[:name],
       email: params[:email],
       role: params[:role],
+      password: params[:password],
     )
     redirect_to "/"
   end
