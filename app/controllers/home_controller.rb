5b471b4ff2ed92274f082ea55f723887d5cfb2d0
SriHarshaSabbineni <sabbinenisriharsha8490@gmail.com>
2020-05-22 21:00:04 +0530

    implemented sessions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def index
-    render "index"
+    if current_user
+      if current_user.role == "costumer"
+        redirect_to costumer_home_path
+      elsif current_user.role == "clerk"
+        redirect_to clerk_home_path
+      elsif current_user.role == "admin"
+        redirect_to admin_home_path
+      end
+    else
+      render "index"
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,26 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:email], role: params[:role])
     if user.authenticate(params[:password])
-      if user.role == "costumer"
-        redirect_to costumer_home_path
-      elsif user.role == "clerk"
-        redirect_to clerk_home_path
-      elsif user.role == "admin"
-        redirect_to admin_home_path
-      end
+      session[:current_user_id] = user.id
+      redirect_to "/"
     end
   end
 
   def costumer_home
-    render "costumer_home"
+    render "costumer_home", locals: { user_name: current_user.name }
   end
 
   def clerk_home
-    render "clerk_home"
+    render "clerk_home", locals: { user_name: current_user.name }
   end
 
   def admin_home
-    render "admin_home"
+    render "admin_home", locals: { user_name: current_user.name }
   end
 end
